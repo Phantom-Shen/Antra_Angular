2692dbe5b3d7341606c5839a652040b841afb913
Phantom-Shen <whateverjackson@gmail.com>
2022-07-23 21:30:20 -0700

    finish add and delete ToDos

--- Assignments/to_do_list/Api/api.js
+++ Assignments/to_do_list/Api/api.js
@@ -2,11 +2,29 @@ export const Api = (() => {
   const baseUrl = "https://jsonplaceholder.typicode.com"
   
   const getTodo = async () => {
-    return await fetch([baseUrl,"todos"].join("/"))
+    return await fetch([baseUrl, "todos"].join("/"))
     .then(res => res.json());
+  }
+
+  const addToDo = async (ToDoNew) => {
+    return await fetch([baseUrl, "todos"].join("/"), {
+      method: "POST",
+      body: JSON.stringify(ToDoNew),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then(res => res.json());
+  }
+
+  const deleteToDO = async (id) => {
+    return await fetch([baseUrl, "todos", id].join("/"), {
+      method: "DELETE",
+    }).then(res => res.json());
   }
 
   return {
     getTodo,
+    addToDo,
+    deleteToDO
   }
 })()
--- Assignments/to_do_list/index.js
+++ Assignments/to_do_list/index.js
@@ -4,7 +4,7 @@ const View = (() => {
 
   const domStr = {
     toDoList: ".to-do-list",
-
+    toDoAdd: "#to-do-add",
   };
 
   const render = (element, children) => {
@@ -17,14 +17,15 @@ const View = (() => {
       li_toDoItem.classList.add("to-do-item");
 
       const span_title = document.createElement("span");
-      span_title.textContent = toDo.title;
+      span_title.textContent = toDo.id + ". " + toDo.title;
 
       const button_delete = document.createElement("button");
       button_delete.classList.add("delete-btn");
+      button_delete.id = toDo.id;
       button_delete.textContent = "X";
 
       li_toDoItem.appendChild(span_title);
-      li_toDoItem.appendChild(button_delete)
+      li_toDoItem.appendChild(button_delete);
       //   <li class="to-do-item">
       //     <span>${toDo.title}</span>
       //     <button class="delete-btn">X</button>
@@ -43,9 +44,17 @@ const View = (() => {
 // -----------------------------------------------------------
 
 const Model = ((api, view) => {
+  class ToDo{
+    constructor(userId, title){
+      this.userId = userId;
+      this.title = title;
+      this.completed = false;
+    }
+  }
 
   class State{
     #toDoList = [];
+    #userId = 2; // mock
 
     get toDoList(){
       return this.#toDoList;
@@ -55,15 +64,22 @@ const Model = ((api, view) => {
       this.#toDoList = returnedToDoList;
       // rerender
       const toDoList = document.querySelector(view.domStr.toDoList);
-      view.render(toDoList, view.createToDoList(this.#toDoList));
+      view.render(toDoList, view.createToDoList(this.#toDoList.filter(item => !item.completed)));
+    }
+
+    get userId(){
+      return this.#userId;
     }
   }
 
-  const {getTodo} = api;
+  const {getTodo, addToDo, deleteToDO} = api;
 
   return{
     State,
+    ToDo,
     getTodo,
+    addToDo,
+    deleteToDO
   }
 
 })(Api, View);
@@ -75,16 +91,35 @@ const Controller = ((model, view) => {
 
   const init = () => {
     model.getTodo().then((returnedToDoList) => {
-      state.toDoList = [...returnedToDoList];
+      state.toDoList = [...returnedToDoList.reverse()];
     });
   };
 
   const addToDo = () => {
+    const toDoAdd = document.querySelector(view.domStr.toDoAdd);
+    toDoAdd.addEventListener("keyup", (event) => {
+      if(event.code === "Enter" && event.target.value.trim() !== ""){
+        const toDoNew = new model.ToDo(state.userId, event.target.value);
 
+        model.addToDo(toDoNew).then(response => {
+          state.toDoList = [response, ...state.toDoList];
+        });
+
+        event.target.value = "";
+      }
+    });
   }
 
   const deleteToDo = () => {
-
+    const toDoList = document.querySelector(view.domStr.toDoList);
+    toDoList.addEventListener("click", (event) => {
+      if(event.target.className === "delete-btn"){
+        state.toDoList = state.toDoList.filter((item) => {
+          return +item.id !== +event.target.id;
+        });
+        model.deleteToDO(event.target.id);
+      }
+    });
   }
 
   const bootstrap = () => {
